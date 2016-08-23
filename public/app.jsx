3daf2453d344aa4639e32d6a92fdf0bb4f2622d2
Sean ODell <amsean.odell@gmail.com>
2016-08-23 14:46:08 -0400

    JSX
    -Input variables etc in jsx

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,26 @@
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'This is the default message!'
+    };
+  },
+  render: function () {
+    var name = this.props.name;
+    var message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+      </div>
+    );
+  }
+});
+
+var firstName = 'Sean';
+
 ReactDOM.render(
-  <h1>Pudding</h1>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
