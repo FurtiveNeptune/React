fe4cafcc3ecc2d6828e623ae6ecc85ec60ef167c
Sean ODell <amsean.odell@gmail.com>
2016-08-23 16:13:21 -0400

    No Blank
    -Improved message insertion by changing the lack of value when no value is inserted

--- public/app.jsx
+++ public/app.jsx
@@ -13,13 +13,16 @@ var Greeter = React.createClass({
   onButtonClick: function (e) {
     e.preventDefault();
 
-    var name = this.refs.name.value;
-    this.refs.name.value = '';
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
 
+    if (typeof name === 'string' && name.length > 0) {
     this.setState({
       name: name
     });
-  },
+  }
+},
   render: function () {
     var name = this.state.name;
     var message = this.props.message;
